4dcf597a8396f84444b758825a3766c932859c0a
Aktan <you@example.com>
2022-10-24 16:46:41 +0600

    FrontController

---
+++ frontController/AppException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\frontController;
+
+use Throwable;
+
+class AppException extends \Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
